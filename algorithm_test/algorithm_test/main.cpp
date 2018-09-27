f2b9d04a70bf40f27a23012695278f80d17757b8
HyunnSangCho <zx251874@naver.com>
2018-09-28 00:49:16 +0900

    14052 commit

---
+++ algorithm_test/algorithm_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm_test
+//
+//  Created by HyunSang Cho on 2018. 9. 27..
+//  Copyright © 2018년 HyunSang Cho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
